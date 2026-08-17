9c485e5ce5a11d56bb3cccadf62262e71ad61996
hanlin2007 <3839585437@qq.com>
2026-08-17 16:37:12 +0800

    F**k. I forget to run git add to make the test modified files work

--- learning/class_trail.py
+++ learning/class_trail.py
@@ -10,10 +10,12 @@ class Dog(Animal):
         super().speak()
 
 dog = Dog()
-dog.speak()
-
-# 是同一个 self 实体！！super.() 的语法糖
+dog.speak()  # dog.speak() 语法糖 = Dog.speak(self)
+# super.speak() 语法糖 = Animal.speak(self)  是同一个 self 实体！！！
 
 # 输出结果：
 # dog setup <__main__.Dog object at 0x7aa8d2ffefc0>
 # animal < __main__.Dog object at 0x7aa8d2ffefc0>
+
+# 这是一个关于 --amend 的测试：在已经 commit 并且 push 到远端仓库之后
+# 如果继续使用 --amend，这个 --amend 的 commit 会在本地 push 之前的 commit 上产生分支，造成本地与远程仓库的分支差异
