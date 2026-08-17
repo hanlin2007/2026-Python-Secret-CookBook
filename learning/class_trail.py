d339f348eb00ac403ff241aeb761589617cbd72f
hanlin2007 <3839585437@qq.com>
2026-08-17 15:41:21 +0800

    Before test, this is a simulation to class & succssion

--- learning/class_trail.py
+++ learning/class_trail.py
@@ -8,8 +8,11 @@ class Dog(Animal):
         print("dog setup", self)
         super().speak()
 
-
 dog = Dog()
 dog.speak()
 
 # 是同一个 self 实体！！super.() 的语法糖
+
+# 输出结果：
+# dog setup <__main__.Dog object at 0x7aa8d2ffefc0>
+# animal < __main__.Dog object at 0x7aa8d2ffefc0>
